ac4399b18919e46f0e2f768f6be73ac0a7908050
Antonio <antonio18casta@aol.com>
2022-05-13 17:55:47 -0400

    commit tree d111c1652200bb1232b1654faf546ed57121bc7d

---
+++ index.js
@@ -0,0 +1,20 @@
+// 1
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+// 2
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+//3
+
+function  introductionWithLanguageOptional(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+//4
+function  introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
